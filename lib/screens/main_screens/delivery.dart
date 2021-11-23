298fd94058bf57debaabb90ea6503364168e6c34
handuah <handuah@gmail.com>
2021-11-23 15:02:08 +0000

    finished the delivery page

--- lib/screens/main_screens/delivery.dart
+++ lib/screens/main_screens/delivery.dart
@@ -29,10 +29,12 @@ class _DeliveryState extends State<Delivery> {
               _TopBar(),
               _StepperIcons(),
               SizedBox(height: screenHeight * 0.02),
-              _DeliveryList(),
+              const _DeliveryList(),
               // _SubTotalCard(),
               SizedBox(height: screenHeight * 0.02),
-              _NextPageBtn(),
+              const _SubtotalList(),
+              SizedBox(height: screenHeight * 0.02),
+              const _NextPageBtn(),
             ],
           ),
         ),
@@ -139,67 +141,6 @@ class _StepperIcons extends StatelessWidget {
   }
 }
 
-class _SubTotalCard extends StatelessWidget {
-  // const _SubTotalCard({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    double screenHeight = MediaQuery.of(context).size.height;
-    double screenWidth = MediaQuery.of(context).size.width;
-    return GestureDetector(
-      child: Container(
-        width: screenWidth,
-        height: screenHeight * 0.14,
-        decoration: BoxDecoration(
-          color: BookFanColors.gray.withOpacity(0.4),
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Courier Delivery',
-                    style: BookTextStyle.bodyTextBlk.copyWith(
-                        color: BookFanColors.dark,
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    height: screenHeight * 0.01,
-                  ),
-                  SizedBox(
-                    width: screenWidth * 0.5,
-                    child: Text(
-                      'Shipping cost GHS 20. Free shipping for orders of GHS 10.',
-                      overflow: TextOverflow.clip,
-                      style: BookTextStyle.bodyTextBlk
-                          .copyWith(color: BookFanColors.dark, fontSize: 12.0),
-                    ),
-                  ),
-                ],
-              ),
-              GestureDetector(
-                child: Icon(
-                  Icons.circle_outlined,
-                  color: BookFanColors.dark.withOpacity(0.6),
-                  size: 30.0,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
 class _NextPageBtn extends StatefulWidget {
   const _NextPageBtn({Key? key}) : super(key: key);
 
@@ -247,70 +188,159 @@ class _DeliveryList extends StatefulWidget {
 }
 
 class __DeliveryListState extends State<_DeliveryList> {
+  List<Map<String, dynamic>>? deliveryCards = [
+    {
+      'deliveryType': 'Courier Delivery',
+      'details': 'Shipping cost GHS 20. Free shipping for orders of GHS 10.',
+      'select': false,
+      'button_index': 1
+    },
+    {
+      'deliveryType': 'Pickup Delivery',
+      'details': 'Allow notifications to know when to pickup your delivery',
+      'select': false,
+      'button_index': 1
+    },
+    {
+      'deliveryType': 'Postal Delivery',
+      'details': 'Shipping cost GHS 200. Free shipping for orders of GHS 10.',
+      'select': false,
+      'button_index': 1
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
-    return Container(
-      height: screenHeight * 0.5,
-      width: screenWidth,
-      color: Colors.white,
-      child: ListView.separated(
-          itemBuilder: (context, index) {
-            return GestureDetector(
-              child: Container(
-                width: screenWidth,
-                height: screenHeight * 0.14,
-                decoration: BoxDecoration(
-                  color: BookFanColors.gray.withOpacity(0.4),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
+    return Column(
+      children: [
+        ...deliveryCards!
+            .map(
+              (cards) => [
+                GestureDetector(
+                  child: Container(
+                    width: screenWidth,
+                    height: screenHeight * 0.13,
+                    decoration: BoxDecoration(
+                      color: BookFanColors.gray.withOpacity(0.4),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          Text(
-                            'Courier Delivery',
-                            style: BookTextStyle.bodyTextBlk.copyWith(
-                                color: BookFanColors.dark,
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.bold),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                cards['deliveryType'] ?? '',
+                                // 'Courier Delivery',
+                                style: BookTextStyle.bodyTextBlk.copyWith(
+                                    color: BookFanColors.dark,
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(
+                                height: screenHeight * 0.01,
+                              ),
+                              SizedBox(
+                                width: screenWidth * 0.5,
+                                child: Text(
+                                  cards['details'] ?? '',
+                                  // 'Shipping cost GHS 20. Free shipping for orders of GHS 10.',
+                                  overflow: TextOverflow.clip,
+                                  style: BookTextStyle.bodyTextBlk.copyWith(
+                                      color: BookFanColors.dark,
+                                      fontSize: 12.0),
+                                ),
+                              ),
+                            ],
                           ),
-                          SizedBox(
-                            height: screenHeight * 0.01,
-                          ),
-                          SizedBox(
-                            width: screenWidth * 0.5,
-                            child: Text(
-                              'Shipping cost GHS 20. Free shipping for orders of GHS 10.',
-                              overflow: TextOverflow.clip,
-                              style: BookTextStyle.bodyTextBlk.copyWith(
-                                  color: BookFanColors.dark, fontSize: 12.0),
+                          GestureDetector(
+                            child: Icon(
+                              Icons.circle_outlined,
+                              color: BookFanColors.dark.withOpacity(0.6),
+                              size: 30.0,
                             ),
                           ),
                         ],
                       ),
-                      GestureDetector(
-                        child: Icon(
-                          Icons.circle_outlined,
-                          color: BookFanColors.dark.withOpacity(0.6),
-                          size: 30.0,
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
-            );
-          },
-          separatorBuilder: (context, index) => Divider(),
-          itemCount: 3),
+                SizedBox(
+                  height: screenHeight * 0.01,
+                ),
+              ],
+            )
+            .expand((element) => element)
+            .toList()
+      ],
+    );
+    // );
+  }
+}
+
+class _SubtotalList extends StatelessWidget {
+  const _SubtotalList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Map<String, dynamic>>? totalList = [
+      {
+        'description': 'Subtotal',
+        'amount': 'GHS 20',
+      },
+      {
+        'description': 'Discount',
+        'amount': 'GHS 20',
+      },
+      {
+        'description': 'Subtotal',
+        'amount': 'GHS 20',
+      },
+      {
+        'description': 'Shipping',
+        'amount': 'GHS 20',
+      },
+      {
+        'description': 'Total',
+        'amount': 'GHS 20',
+      },
+    ];
+    return Column(
+      children: [
+        ...totalList
+            .map(
+              (list) => [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "${list['description']} :",
+                      style: BookTextStyle.heading_3.copyWith(
+                          fontSize: 15.0, fontWeight: FontWeight.w200),
+                    ),
+                    Text(
+                      list['amount'],
+                      style: BookTextStyle.heading_3.copyWith(
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.w200,
+                          color: BookFanColors.gray),
+                    ),
+                  ],
+                ),
+                Divider(),
+              ],
+            )
+            .expand((element) => element)
+            .toList()
+      ],
     );
   }
 }
